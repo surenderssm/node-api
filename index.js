6a4777ac96e025ba094df0533fff433cd7e2bec3
Surender Singh Malik <surenderssm@users.noreply.github.com>
2025-01-24 23:07:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // filepath: /D:/github-repos/appservice-linux-samples/nodejs/myapi/index.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
